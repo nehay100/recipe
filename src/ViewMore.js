a49c3df6b6402d768e8606aafe4ee15b18406996
liny55@uw.edu <liny55@uw.edu>
2018-03-13 16:26:49 -0700

    ViewMore

--- src/ViewMore.js
+++ src/ViewMore.js
@@ -23,16 +23,22 @@ class ViewMore extends Component {
     render() {
         return (
             <div className="card mt-8" width="18rem">
-                <h1>ViewMore</h1>
-                {/* <img className="card-img-top" src={this.state.recipe.imgURL} alt="Card image cap" /> */}
+                <h1>More Information!</h1>
+                <img className="card-img-top" src={this.state.recipe.imgURL} alt="Card image cap" />
                 <div className="card-body">
-                    {/* <h5 className="card-title">{this.state.recipe.title}</h5>
-                    <p className="card-text d-inline mr-3">{"Author: " + this.state.recipe.author}</p>
-                    <p className="card-text d-inline mr-3">{"Calories: " + this.state.recipe.calories}</p>
-                    <p className="card-text d-inline mr-3">{"Duration: " + this.state.recipe.duration}</p>
-                    <p className="card-text mt-3">{this.state.recipe.description}</p>
-                    <p className="card-text">{"Ingredients: " + this.state.recipe.ingredients}</p> */}
+                    <h5 className="card-title">{this.state.recipe.title}</h5>
+                    <p className="card-subtitle mb-2 text-muted">{this.state.recipe.description}</p>
+                    <p className="card-text">{"Author: " + this.state.recipe.author}</p>
+                    <p className="card-text">{"Calories: " + this.state.recipe.calories}</p>
+                    <p className="card-text">{"Duration: " + this.state.recipe.duration + " minutes"}</p>
+                    <p className="card-text">{"Ingredients: " + this.state.recipe.ingredients}</p>
+
+                    <p className="card-text">{"Genres: " + this.state.recipe.genres}</p>
+                    <p className="card-text">{"Total Likes: " + this.state.recipe.likes}</p>
+                    <p className="card-text">{"Ratings: " + this.state.recipe.ratings}</p>
+                    <p className="card-text">{"Allergies: " + this.state.recipe.allergies}</p>
                     <a href="#/steps" className="btn btn-primary">Steps</a>
+                    {/* <a href="#/steps" className="btn btn-primary" onClick={() => this.props.updateSelectedRecipe(this.props.recipeKey)}>Steps</a> */}
                 </div>
             </div>
         );
